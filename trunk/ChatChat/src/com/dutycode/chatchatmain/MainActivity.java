7c4c331aa5c8163a8b378e1f965e86ec1dafd5a5
dutycode@gmail.com <dutycode@gmail.com@e1948457-a120-1ce7-a3c2-4104eab391fb>
2013-04-13 08:27:00 +0000

    添加在线状态

--- trunk/ChatChat/src/com/dutycode/chatchatmain/MainActivity.java
+++ trunk/ChatChat/src/com/dutycode/chatchatmain/MainActivity.java
@@ -6,6 +6,9 @@ import java.util.Map;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -20,7 +23,6 @@ import android.widget.ExpandableListView.OnChildClickListener;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.dutycode.service.ChatService;
 import com.dutycode.service.ClientConServer;
 
 public class MainActivity extends Activity {
@@ -61,7 +63,6 @@ public class MainActivity extends Activity {
 		ex_listview_friendlist
 				.setAdapter(new ExpandListViewFriendListAdapter());
 
-	
 		// 添加点击用户事件。点击用户进入到发送消息界面
 		ex_listview_friendlist
 				.setOnChildClickListener(new OnChildClickListener() {
@@ -70,24 +71,25 @@ public class MainActivity extends Activity {
 					public boolean onChildClick(ExpandableListView parent,
 							View v, int groupPosition, int childPosition,
 							long id) {
-						String username = childArr.get(groupPosition).get(childPosition).toString();
-						String userJID = new ClientConServer().getUserJIDByName(username);
-						
-						//用于传递参数到下一个Activity
+						String username = childArr.get(groupPosition)
+								.get(childPosition).toString();
+						String userJID = new ClientConServer()
+								.getUserJIDByName(username);
+
+						// 用于传递参数到下一个Activity
 						Bundle bundle = new Bundle();
 						bundle.putString("userJID", userJID);
-						Intent intent = new Intent(MainActivity.this, ChatActivity.class);
+						Intent intent = new Intent(MainActivity.this,
+								ChatActivity.class);
 						intent.putExtras(bundle);
 						startActivity(intent);
 						return false;
 					}
 				});
-		
-		
-		
-		/*添加消息监听*/
-//		new ChatService().listenningMessage();
-		
+
+		/* 添加消息监听 */
+		// new ChatService().listenningMessage();
+
 	}
 
 	@Override
@@ -113,10 +115,10 @@ public class MainActivity extends Activity {
 					.show();
 			mHandler.sendEmptyMessageDelayed(0, 2000);
 		} else {
-			
-			//将登陆状态改为退出登陆
+
+			// 将登陆状态改为退出登陆
 			new ClientConServer().logoff();
-			//返回主界面
+			// 返回主界面
 			Intent intent = new Intent(Intent.ACTION_MAIN);
 			intent.addCategory(Intent.CATEGORY_HOME);
 			startActivity(intent);
@@ -164,21 +166,30 @@ public class MainActivity extends Activity {
 						.get(childPosition).toString());
 			}
 
+			Drawable img_online, img_offline;
+
+			Resources res = getResources();
+
+			img_online = res.getDrawable(R.drawable.online);
+			img_offline = res.getDrawable(R.drawable.offline);
+
+			// 调用setCompoundDrawables时，必须调用Drawable.setBounds()方法,否则图片不显示
+
+			img_online.setBounds(0, 0, img_online.getMinimumWidth(), img_online.getMinimumHeight());
+			img_offline.setBounds(0, 0, img_offline.getMinimumWidth(), img_offline.getMinimumHeight());
+
+
 			/**
 			 * 判断用户在线状态未完成！
 			 */
-			// System.out.println("Mode : " + new
-			// ClientConServer().getMode(childArr.get(groupPosition).get(childPosition)
-			// .toString()));
-			// //判断是否在线
-			// if (new
-			// ClientConServer().isSomeOneOnline(childArr.get(groupPosition).get(childPosition)
-			// .toString())){
-			//
-			// text.setBackgroundColor(Color.RED);
-			// }else {
-			// text.setBackgroundColor(Color.BLUE);
-			// }
+			// 判断是否在线
+			if (new ClientConServer().isSomeOneOnline(childArr
+					.get(groupPosition).get(childPosition).toString())) {
+
+				text.setCompoundDrawables(img_online, null, null, null);
+			} else {
+				text.setCompoundDrawables(img_offline, null, null, null);
+			}
 			return text;
 			/*
 			 * 下面这段代码会导致列表错乱，具体原因现在还没有找到 LinearLayout ll = null; if (convertView
@@ -229,7 +240,7 @@ public class MainActivity extends Activity {
 
 		@Override
 		public boolean isChildSelectable(int arg0, int arg1) {
-			//返回true保证子列表能够被点击，若返回false，则不能被点击
+			// 返回true保证子列表能够被点击，若返回false，则不能被点击
 			return true;
 		}
 

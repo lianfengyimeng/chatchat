36181dbc1a5e8a9ff90fdb01a14b5d8250396750
dutycode@gmail.com <dutycode@gmail.com@e1948457-a120-1ce7-a3c2-4104eab391fb>
2013-04-15 07:55:51 +0000

    聊天基本功能完成，聊天记录保存功能完成，添加返回保存功能

--- trunk/ChatChat/src/com/dutycode/service/ChatService.java
+++ trunk/ChatChat/src/com/dutycode/service/ChatService.java
@@ -1,10 +1,12 @@
 package com.dutycode.service;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -17,13 +19,17 @@ import org.jivesoftware.smack.XMPPException;
 import org.jivesoftware.smack.packet.Message;
 
 import android.content.Context;
+import android.os.Handler;
 import android.util.Log;
 import android.widget.SimpleAdapter;
 
 import com.dutycode.bean.MessageBean;
+import com.dutycode.chatchatmain.ChatActivity;
 import com.dutycode.chatchatmain.MainActivity;
 import com.dutycode.chatchatmain.R;
+import com.dutycode.configdata.Fileconfig;
 import com.dutycode.configdata.MessageConfig;
+import com.dutycode.tool.AndroidTools;
 
 
 /**
@@ -40,9 +46,27 @@ public class ChatService {
 	
 	/*用户放置聊天信息*/
 	private List<Map<String,Object>> list = new ArrayList<Map<String,Object>>();
+	//消息监听器
+	private NewMessageListener messagelistener = new NewMessageListener();
 	
+	//消息Handler，用于和主线程UI进行交互，刷新UI数据
+	private Handler messageListenHandler;
+	
+	//chatmanger用于处理当前的聊天
+	private ChatManager chatmanger = connection.getChatManager();
+	
+	private Chat chat ;
 	
 	SimpleDateFormat sdf = new SimpleDateFormat("MM-dd HH:mm:ss");
+	
+	SimpleDateFormat sdfdate = new SimpleDateFormat("yyyy-MM-dd");
+	
+	
+	public ChatService(){}
+	
+	public ChatService(Handler _handler){
+		this.messageListenHandler = _handler;
+	}
 	
 	/**
 	 * 发送消息（简单消息，不包括附加内容）
@@ -50,27 +74,23 @@ public class ChatService {
 	 * @param _message 发送的消息内容
 	 */
 	public void sendMessage(String _userJID, String _message){
-		ChatManager chatmanger = connection.getChatManager();
 		String chatThreadId = _userJID;
-		
-		Chat newChat = null;
 		
 		//判断ChatThread是否存在
 		if (chatmanger.getThreadChat(chatThreadId) != null){
-			newChat = chatmanger.getThreadChat(chatThreadId);
+			chat = chatmanger.getThreadChat(chatThreadId);
 		}else {
-			newChat = chatmanger.createChat(_userJID, chatThreadId, null);
+			chat = chatmanger.createChat(_userJID, chatThreadId, null);
 		}
 		
 		MessageBean messageBean = new MessageBean();
 		
 		try {
-			newChat.sendMessage(_message);
+			chat.sendMessage(_message);
 			
 			//记录消息内容，保存到消息的List中
 			messageBean.setMessageBody(_message);
 			messageBean.setMessageFrom(MainActivity.userloginname + "  ");
-//			messageBean.setMessageTo(_userJID);
 			messageBean.setMessageTime(sdf.format(new Date()));
 			
 			logMessage(messageBean);
@@ -86,18 +106,16 @@ public class ChatService {
 	 * @param _userJID 用户的JID，这里同时为聊天的ThreadID
 	 */
 	public void listenningMessage(String _userJID){
-		ChatManager chatmanger = connection.getChatManager();
-		
-		Chat chat = chatmanger.getThreadChat(_userJID);
 
 		String chatThread = _userJID;
-		NewMessageListener messagelistener = new NewMessageListener();
+
 		if (chatmanger.getThreadChat(chatThread) != null){
 			chat = chatmanger.getThreadChat(chatThread);
 			chat.addMessageListener(messagelistener);
 		}else {
 			chat = chatmanger.createChat(_userJID, chatThread, messagelistener);
 		}
+		
 		
 	}
 	
@@ -123,13 +141,16 @@ public class ChatService {
 	private void logMessage(MessageBean _message){
 		if (messageList.size() == MessageConfig.MESSAGE_MAX_LENGTH){
 			//messageList到达最大长度,将内容保存到文件中
+			saveMessage(messageList);
+			Log.i("listSizeIn:", "" + messageList.size());
+			//将messageList置为空
+			messageList.removeAll(messageList);
+			messageList.add(_message);
+			Log.i("listSizeIn2:", "" + messageList.size());
 		}else {
 			messageList.add(_message);
 			Log.i("listsize", "" + messageList.size());
 		}
-		
-		
-	
 		
 	}
 	
@@ -137,52 +158,110 @@ public class ChatService {
 	 * 将消息保存到内存卡文件当中
 	 * @param _messageList 
 	 */
-	private void saveMessageToFile(List<Message> _messageList){
+	private void saveMessage(List<MessageBean> _messageList){
+		
+		//聊天的记录文件名
+		String messageLogName = sdfdate.format(new Date()) + ".chatchatfile";
+		//聊天记录文件所在位置的文件夹，这里格式是这样的:chatchat/messagelog/UserName/：其中UserName为当前登录用户名
+		String messageFolder = MessageConfig.MESSAGE_LOG_PATH + MainActivity.userloginname +"/"+chat.getThreadID() +"/";
+		//聊天记录完整路径
+		String messageFinalLogPath = Fileconfig.sdrootpath + messageFolder + messageLogName;
+		
+		//检测是否存在SD卡，如果不存在,不能保存聊天记录
+		if (AndroidTools.isHasSD()){
+			//存在SD卡，执行存储任务
+			if (!AndroidTools.isFileExists(messageFinalLogPath)){
+				//文件不存在，创建文件，并添加内容
+				AndroidTools.createFileOnSD(messageFolder, messageLogName);
+			}
+			//保存消息记录
+			saveMessageToFile(messageList, messageFinalLogPath);
+		}
 		
 	}
 	
 	
-	public SimpleAdapter getMessageListAdapter(Context _context){
+	/**
+	 * 文件保存方法，将消息保存到文件当中
+	 * @param _messageList 消息列表
+	 * @param _filepath 文件路径
+	 */
+	private void saveMessageToFile(List<MessageBean> _messageList, String _filepath){
+		StringBuffer messagesb = new StringBuffer();
 		
-		/*用于放置聊天详细内容*/
-		Map<String,Object> map = new HashMap<String, Object>();
+		
+		messagesb.append("---------MessageSaved at ：" + sdf.format(new Date()) + "---------\n");
+		//将消息内容转换成String类型用于保存到文件中
+		for (int i = 0; i < _messageList.size(); i++){
+			messagesb.append(messageList.get(i).getMessageFrom() + "\n");
+			messagesb.append(messageList.get(i).getMessageTime() + "\n");
+			messagesb.append(messageList.get(i).getMessageBody() + "\n");
+			messagesb.append("\n");
+		}
+		try {
+			FileOutputStream fout = new FileOutputStream(_filepath,true);
+			byte[] bytes = messagesb.toString().getBytes();
+			
+			fout.write(bytes);
+			fout.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
+	/**
+	 * 仅用于在点击返回按钮的时候进行检查是否需要存文件
+	 */
+	public void onlySaveMessageFileOnExit(){
+		if (messageList.size() < MessageConfig.MESSAGE_MAX_LENGTH){
+			
+			//聊天的记录文件名
+			String messageLogName = sdfdate.format(new Date())  + ".chatchatfile";
+			//聊天记录文件所在位置的文件夹，这里格式是这样的:chatchat/messagelog/UserName/：其中UserName为当前登录用户名
+			String messageFolder = MessageConfig.MESSAGE_LOG_PATH + MainActivity.userloginname +"/" + chat.getThreadID() + "/";
+			//聊天记录完整路径
+			String messageFinalLogPath = Fileconfig.sdrootpath + messageFolder + messageLogName;
+			
+			saveMessageToFile(messageList, messageFinalLogPath);
+		}
+	}
+
+	/**
+	 * 得到聊天内容的List
+	 * @return
+	 */
+	public List<Map<String,Object>> getMessageList(){
+		
+		Map<String,Object> messagemap = new HashMap<String, Object>();
 		
 		int listsize = messageList.size();
 		if (listsize>0){
-			map.put("messageBody", messageList.get(listsize-1).getMessageBody());
-			map.put("messageFrom", messageList.get(listsize-1).getMessageFrom() + "  ");
-			map.put("messageTime", messageList.get(listsize-1).getMessageTime());
+			messagemap.put("messageBody", messageList.get(listsize-1).getMessageBody());
+			messagemap.put("messageFrom", messageList.get(listsize-1).getMessageFrom() + "  ");
+			messagemap.put("messageTime", messageList.get(listsize-1).getMessageTime());
 			
-			list.add(map);
+			list.add(messagemap);
 		}
-		
-		SimpleAdapter simpleAdapter = new SimpleAdapter(_context,list,R.layout.chat_list,
-				new String[]{"messageFrom","messageBody","messageTime"},
-				new int[]{R.id.messageFrom, R.id.messageBody, R.id.messageTime});
-		
-		
-		return simpleAdapter;
+		return list;
 	}
+
 	
 	/**
-	 * 放置内容到Adapter中
+	 * 放置内容到Adapter中,重载
 	 */
 	private void setAdapterList(){
-		/*用于放置聊天详细内容*/
-		Map<String,Object> map = new HashMap<String, Object>();
 		
+		/*更新UI主线程，刷新聊天列表*/
 		
-		int listsize = messageList.size();
-		if (listsize > 0){
-			map.put("messageBody", messageList.get(listsize-1).getMessageBody());
-			map.put("messageFrom", messageList.get(listsize-1).getMessageFrom() + "   ");
-			map.put("messageTime", messageList.get(listsize-1).getMessageTime());
-//			map.put("messageTo", messageList.get(listsize-1).getMessageTo());
-			
-			list.add(map);
-		}
-		
-		
+		android.os.Message message = android.os.Message.obtain();
+		message.obj = this.getMessageList();
+		messageListenHandler.sendMessage(message);
 		
 	}
 	
@@ -192,20 +271,20 @@ public class ChatService {
 	 *
 	 */
 	class NewMessageListener implements MessageListener{
-
+		
+		public NewMessageListener(){}
+		
+		//消息bean
 		
 		@Override
 		public void processMessage(Chat chat, Message message) {
-			System.out.println("------:::::::::::::::::::::::::::::::::::::::::;");
-			
 			MessageBean messageBean = new MessageBean();
 			messageBean.setMessageBody(message.getBody());
 			messageBean.setMessageFrom(message.getFrom());
-//			messageBean.setMessageTo(message.getTo());
 			messageBean.setMessageTime("("+sdf.format(new Date())+")");
 			
 			logMessage(messageBean);
-//			setAdapterList();
+			setAdapterList();
 		}
 		
 		

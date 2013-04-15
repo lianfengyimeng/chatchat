ce12148a60e5abf9c52636e10438766546c0d0b9
dutycode <dutycode@gmail.com>
2013-04-15 07:57:32 +0000

    添加消息存储路径常量

--- ChatChat/src/com/dutycode/configdata/MessageConfig.java
+++ ChatChat/src/com/dutycode/configdata/MessageConfig.java
@@ -11,4 +11,14 @@ public class MessageConfig {
 	 */
 	public static int MESSAGE_MAX_LENGTH = 10 ;
 	
+	/**
+	 * 消息存放位置相对路径
+	 */
+	public static String MESSAGE_LOG_PATH = "chatchat/messagelog/";
+	
+	/**
+	 * 消息存放位置绝对路径（加上内存卡的位置）
+	 */
+	public static String MESSAGE_LOG_FINAL_PATH = Fileconfig.sdrootpath + MESSAGE_LOG_PATH;
+	
 }

5b2a78909bac8e1c6a94ed1be2614cf73a44227b
dutycode@gmail.com <dutycode@gmail.com@e1948457-a120-1ce7-a3c2-4104eab391fb>
2013-04-26 05:33:49 +0000

    文件传输监听--在具体的聊天界面中完成

--- trunk/ChatChat/src/com/dutycode/configdata/Fileconfig.java
+++ trunk/ChatChat/src/com/dutycode/configdata/Fileconfig.java
@@ -4,11 +4,12 @@ import java.io.File;
 
 /**
  * 文件相关配置变量
+ * 
  * @author michael
- *
+ * 
  */
 public class Fileconfig {
-	
+
 	/**
 	 * 保存密码的xml文件名称，存储在SD上
 	 */
@@ -17,10 +18,20 @@ public class Fileconfig {
 	 * 保存的xml文件所在文件夹
 	 */
 	public final static String xmlfolderpath = "chatchat/";
-	
+
 	public final static String xmlinfopath = xmlfolderpath + xmlinfoname;
 	/**
 	 * sd卡根路径
 	 */
-	public final static String sdrootpath = android.os.Environment.getExternalStorageDirectory() + File.separator;
+	public final static String sdrootpath = android.os.Environment
+			.getExternalStorageDirectory() + File.separator;
+	
+	/**
+	 * 收到的文件保存的位置
+	 */
+	public final static String REVICE_FOLDER = "recivefile/";
+	/**
+	 * 收到的文件保存的绝对位置
+	 */
+	public final static String RECIVE_FILE_FOLDER_PATH =  xmlfolderpath + REVICE_FOLDER;
 }
